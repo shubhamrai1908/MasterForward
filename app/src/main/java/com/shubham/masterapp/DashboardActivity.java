09e16baab0a55dcb6d5fa4ac90b54d76df35d6e3
Shubham Rai <shubhamrai1908@gmail.com>
2021-05-24 15:23:30 +0530

    initial commit3

--- app/src/main/java/com/shubham/masterapp/DashboardActivity.java
+++ app/src/main/java/com/shubham/masterapp/DashboardActivity.java
@@ -1,29 +1,71 @@
 package com.shubham.masterapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
-    Button button;
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+       Toolbar toolbar=findViewById(R.id.toolbar);
+
+        nav=findViewById(R.id.navmenu);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+       nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.menu_pending_req:
+                        Toast.makeText(getApplicationContext(),"pending req",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.menu_ground_rec:
+                        Toast.makeText(getApplicationContext(),"Ground Received",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.menu_server_sent:
+                        Toast.makeText(getApplicationContext(),"Server Sent",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.menu_server_ack:
+                        Toast.makeText(getApplicationContext(),"Server Reply",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.menu_ground_ack:
+                        Toast.makeText(getApplicationContext(),"Ground Ack",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
             }
         });
+
     }
 }
--- app/src/main/java/com/shubham/masterapp/MainActivity.java
+++ app/src/main/java/com/shubham/masterapp/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         }
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
 
     }
 
